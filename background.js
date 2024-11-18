94c2ab5ce3eb3d8313572849c852525437930cfa
simplysaru <saru@simplysaru.com>
2024-11-18 12:04:56 +0530

    added keyboard shortcuts

--- background.js
+++ background.js
@@ -16,3 +16,21 @@ chrome.action.onClicked.addListener(async (tab) => {
         });
 
 });
+chrome.commands.onCommand.addListener(async (tab) => {
+    function getSelections() {
+         selections = getSelection().toString(); return selections;
+     }
+     chrome.scripting
+         .executeScript({
+             target: { tabId: tab.id, allFrames: true },
+             func: getSelections,
+         })
+         .then(injectionResults => {
+             for (const { frameId, result } of injectionResults) {
+                 console.log(`Frame ${frameId} result:`, result);
+                 var thingsURL = ('things:///add?show-quick-entry=true&title=' + encodeURIComponent(tab.title) + '&notes=' + encodeURIComponent(result + " " + tab.url));
+                 chrome.tabs.update({ url: thingsURL });
+             }
+         });
+ 
+ });
